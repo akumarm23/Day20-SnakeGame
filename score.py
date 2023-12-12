bf0d14ac5781ea43222c712f00b9fb902888b5c4
akumarm23 <ak.muniswamy2023@gmail.com>
2023-12-12 17:46:59 +0530

    Second commit

--- score.py
+++ score.py
@@ -8,7 +8,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         # Set up Turtle attributes for score display
         self.color("white")
         self.hideturtle()
@@ -21,15 +22,23 @@ class Score(Turtle):
 
     def update_score(self):
         """Update the score display on the screen."""
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()  # Clear the previous score display
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
 
-    def game_over(self):
-        """Display 'GAME OVER' message on the screen."""
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
+        
+    # def game_over(self):
+    #     """Display 'GAME OVER' message on the screen."""
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGN, font=FONT)
 
     def increase_score(self):
         """Increase the score by 1 and update the score display."""
         self.score += 1
-        self.clear()  # Clear the previous score display
         self.update_score()  # Update the score display with the new score
--- snake.py
+++ snake.py
@@ -58,6 +58,13 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         """Extend the snake by adding a new segment to its tail."""
         self.add_segment(self.segments[-1].position())
